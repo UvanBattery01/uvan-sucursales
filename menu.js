212c9db6af419620b82113b076dd5f02ea578a1e
UvanBattery01 <ocanasebastian56@gmail.com>
2026-07-09 12:32:30 -0600

    Actualizar menu.js

--- menu.js
+++ menu.js
@@ -1,1 +1,21 @@
-/
+const menuBtn = document.getElementById("menuBtn");
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+
+function abrirMenu() {
+    sidebar.classList.add("open");
+    overlay.classList.add("show");
+}
+
+function cerrarMenu() {
+    sidebar.classList.remove("open");
+    overlay.classList.remove("show");
+}
+
+menuBtn.addEventListener("click", abrirMenu);
+
+overlay.addEventListener("click", cerrarMenu);
+
+document.querySelectorAll("#sidebar a").forEach(link=>{
+    link.addEventListener("click", cerrarMenu);
+});
