d8bf35c80e99737252a735944f451e69ec7f1ef7
UvanBattery01 <ocanasebastian56@gmail.com>
2026-07-09 11:42:10 -0600

    Actualizar script.js

--- script.js
+++ script.js
@@ -284,3 +284,23 @@ const btnBuscar=document.getElementById("buscarSucursal");
 if(btnBuscar){
 btnBuscar.addEventListener("click",buscarSucursalMasCercana);
 }
+
+// ================= MENÚ LATERAL =================
+
+const menuBtn = document.getElementById("menuBtn");
+const sidebar = document.getElementById("sidebar");
+const overlay = document.getElementById("overlay");
+
+if (menuBtn && sidebar && overlay) {
+
+    menuBtn.addEventListener("click", () => {
+        sidebar.classList.add("open");
+        overlay.classList.add("show");
+    });
+
+    overlay.addEventListener("click", () => {
+        sidebar.classList.remove("open");
+        overlay.classList.remove("show");
+    });
+
+}
