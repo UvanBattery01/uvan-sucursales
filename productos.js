0082a9c0cff1ce7d38b0609a457a2f33b1d2feed
UvanBattery01 <ocanasebastian56@gmail.com>
2026-07-17 16:06:48 -0600

    Crear productos.js

---
+++ productos.js
@@ -0,0 +1,39 @@
+const productos = [
+
+{
+    id:1,
+    marca:"LTH",
+    linea:"Evolution AGM",
+    modelo:"L-47 (LN2)-660 AGM",
+    categoria:"automovil",
+
+    bci:"47",
+    voltaje:"12 V",
+    cca:"660",
+    ca:"825",
+    ah:"60 Ah",
+    rc:"100 min",
+
+    garantia:"48 meses",
+    reemplazo:"18 meses",
+
+    polaridad:"(-) / (+)",
+
+    medidas:{
+        largo:"241 mm",
+        ancho:"175 mm",
+        alto:"190 mm"
+    },
+
+    peso:"17.35 kg",
+
+    imagenes:[
+        "1000056332.png",
+        "1000076225.png",
+        "lth47-3.png"
+    ],
+
+    descripcion:"Batería AGM diseñada para vehículos con sistema Start-Stop y alta demanda eléctrica."
+}
+
+];
