d3ac6b84ce00a988eae4bfe04c09441eefac01b9
UvanBattery01 <ocanasebastian56@gmail.com>
2026-07-29 21:25:41 -0600

    Actualizar script.js

--- tienda/js/script.js
+++ tienda/js/script.js
@@ -1,1 +1,22 @@
-/
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+
+let actual = 0;
+
+function cambiarSlide(){
+
+    slides[actual].classList.remove("activo");
+    dots[actual].classList.remove("activo");
+
+    actual++;
+
+    if(actual >= slides.length){
+        actual = 0;
+    }
+
+    slides[actual].classList.add("activo");
+    dots[actual].classList.add("activo");
+
+}
+
+setInterval(cambiarSlide,5000);
