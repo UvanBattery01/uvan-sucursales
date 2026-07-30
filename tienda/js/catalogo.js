ac3e8b2ea99242c06053b8ce7a5143083896a320
UvanBattery01 <ocanasebastian56@gmail.com>
2026-07-29 20:57:19 -0600

    Actualizar catalogo.js

--- tienda/js/catalogo.js
+++ tienda/js/catalogo.js
@@ -1,1 +1,25 @@
-.
+const contenedor = document.querySelector(".productos");
+
+if(contenedor){
+
+    productos.forEach(producto=>{
+
+        contenedor.innerHTML += `
+        <div class="producto">
+
+            <img src="${producto.imagen}" alt="${producto.modelo}">
+
+            <h3>${producto.marca}</h3>
+
+            <h4>${producto.modelo}</h4>
+
+            <p>$${producto.precio}</p>
+
+            <button>Agregar al carrito</button>
+
+        </div>
+        `;
+
+    });
+
+}
