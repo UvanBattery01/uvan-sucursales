d4721a5b1267af18f0c751191a2cac524993468f
UvanBattery01 <ocanasebastian56@gmail.com>
2026-07-29 20:57:02 -0600

    Actualizar productos.js

--- tienda/js/productos.js
+++ tienda/js/productos.js
@@ -1,1 +1,27 @@
-.
+const productos = [
+
+{
+    id:1,
+    marca:"GONHER",
+    modelo:"GP-47 AGM",
+    precio:2599,
+    imagen:"img/gp47.png",
+    garantia:"5 años",
+    categoria:"AGM",
+    stock:15,
+    descripcion:"Batería AGM de alto desempeño para vehículos con sistema Start-Stop."
+},
+
+{
+    id:2,
+    marca:"UVAN",
+    modelo:"Premium 47",
+    precio:2199,
+    imagen:"img/gp47.png",
+    garantia:"12 meses",
+    categoria:"Convencional",
+    stock:20,
+    descripcion:"Batería UVAN de excelente rendimiento para uso diario."
+}
+
+];
