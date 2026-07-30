2fc8ce3d9342e96d8889d85a99ed7921bfda07f1
UvanBattery01 <ocanasebastian56@gmail.com>
2026-07-30 13:01:07 -0600

    Actualizar productos.js

--- tienda/js/productos.js
+++ tienda/js/productos.js
@@ -1,22 +1,45 @@
 const productos = [
 
 {
-    id:1,
-    marca:"GONHER",
-    modelo:"GP-47 AGM",
-    precio:4500,
-    imagen:"img/gp47.png",
-    garantia:"5 años",
-    categoria:"AGM",
-    stock:15,
-    descripcion:"Batería AGM de alto desempeño para vehículos con sistema Start-Stop.",
-especificaciones: [
-    "CCA: 660",
-    "Voltaje: 12V",
-    "BCI: 47",
-    "Tecnología AGM",
-    "Libre de mantenimiento"
-]
+    id: 1,
+    marca: "GONHER",
+    modelo: "GP-47 AGM",
+
+    precio: 4500,
+
+    categoria: "AGM",
+
+    imagen: "img/gp47.png",
+
+    garantia: "5 años",
+
+    stock: 15,
+
+    descripcion: "Batería AGM de alto desempeño para vehículos con sistema Start-Stop.",
+
+    cca: 660,
+    bci: 47,
+    voltaje: "12V",
+    tecnologia: "AGM",
+    mantenimiento: "Libre de mantenimiento",
+
+    largo: 242,
+    ancho: 175,
+    alto: 190,
+
+    peso: "18 kg",
+
+    envio: true,
+    instalacion: true,
+    mesesSinIntereses: true,
+
+    especificaciones: [
+        "CCA: 660",
+        "Voltaje: 12V",
+        "BCI: 47",
+        "Tecnología AGM",
+        "Libre de mantenimiento"
+    ]
 },
 
 {
